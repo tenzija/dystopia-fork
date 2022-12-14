74308f01beca8cd2f931f206cd04c523942cce54
Tenzija <48459321+tenzija@users.noreply.github.com>
2022-12-14 14:56:29 +0100

    Update Dyst.sol

--- contracts/contracts/base/token/Dyst.sol
+++ contracts/contracts/base/token/Dyst.sol
@@ -12,25 +12,25 @@ contract Dyst is IERC20 {
 	mapping(address => mapping(address => uint)) public allowance;
 	address public minter;
 
-	constructor() default {
+	constructor() {
 		minter = msg.sender;
 		_mint(msg.sender, 0);
 	}
 
 	function setMinter(address _minter) external {
-		require(msg.sender == minter, "DYST: Not minter");
+		require(msg.sender == minter, "OMNI: Not minter");
 		minter = _minter;
 	}
 
 	function approve(address _spender, uint _value) external override returns(bool) {
-		require(_spender != address(0), "DYST: Approve to the zero address");
+		require(_spender != address(0), "OMNI: Approve to the zero address");
 		allowance[msg.sender][_spender] = _value;
 		emit Approval(msg.sender, _spender, _value);
 		return true;
 	}
 
 	function _mint(address _to, uint _amount) internal returns(bool) {
-		require(_to != address(0), "DYST: Mint to the zero address");
+		require(_to != address(0), "OMNI: Mint to the zero address");
 		balanceOf[_to] += _amount;
 		totalSupply += _amount;
 		emit Transfer(address(0x0), _to, _amount);
@@ -38,9 +38,9 @@ contract Dyst is IERC20 {
 	}
 
 	function _transfer(address _from, address _to, uint _value) internal returns(bool) {
-		require(_to != address(0), "DYST: Transfer to the zero address");
+		require(_to != address(0), "OMNI: Transfer to the zero address");
 		uint fromBalance = balanceOf[_from];
-		require(fromBalance >= _value, "DYST: Transfer amount exceeds balance");
+		require(fromBalance >= _value, "OMNI: Transfer amount exceeds balance");
 
 		balanceOf[_to] += _value;
 		emit Transfer(_from, _to, _value);
@@ -55,14 +55,14 @@ contract Dyst is IERC20 {
 		address spender = msg.sender;
 		uint spenderAllowance = allowance[_from][spender];
 		if (spenderAllowance != type(uint).max) {
-			require(spenderAllowance >= _value, "DYST: Insufficient allowance");
+			require(spenderAllowance >= _value, "OMNI: Insufficient allowance");
 
 		}
 		return _transfer(_from, _to, _value);
 	}
 
 	function mint(address account, uint amount) external returns(bool) {
-		require(msg.sender == minter, "DYST: Not minter");
+		require(msg.sender == minter, "OMNI: Not minter");
 		_mint(account, amount);
 		return true;
 	}
